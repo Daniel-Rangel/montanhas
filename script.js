aee559aac519ac81fb002abbaa12b54fdd9bf187
DanielRangel <danf.arangel@gmail.com>
2021-01-18 23:29:13 -0300

    testes de click com troca de img

--- script.js
+++ script.js
@@ -10,18 +10,27 @@ const montes = [
         img: './img/montanha.jpeg'
     },
     {
-        nome: 'Montes',
+        nome: 'Monte',
         descricao:'Montanha Lore ipsum dolor sit amet consectetur adipisicing elit. Earum odit, reprehenderit veritatis placeat iste beatae saepe neque quod, non modi expedita, laudantium similique iure perferendis ut. Repellat nesciunt consequatur libero?' ,
         img: './img/monte.jpeg'
     }
 ]
 
-const litas = document.querySelectorAll(".menu__item")
+const listas = document.querySelectorAll(".menu__item")
 let img = document.querySelector(".img")
-let titulo = document.querySelector(".artigo_title").innerHTML
+let titulo = document.querySelector(".artigo_title")
 
-for(let item of litas){
+for(let item of listas){
+    
     item.addEventListener("click", () => {
-        console.log(item.innerHTML)
+        for(let mt of montes){
+            if(mt.nome.toLocaleLowerCase == item.innerHTML.toLocaleLowerCase ){
+                document.querySelector('.img').src = mt.img
+                console.log(mt.img)
+            }
+        }        
     })
 }
+
+ /* document.querySelector('.img').src = mt.src
+            document.querySelector('.artigo_title').innerHTML = mt.nome */
